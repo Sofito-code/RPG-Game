9e3e0249c50948faf5d6d036cfc1125c42a5cf45
Sofia Vanegas Cordoba <sofia.vanegasc@udea.edu.co>
2025-11-25 21:14:36 -0500

    Controlador de movimiento y ajuste de escenario

---
+++ Assets/Scripts/Monobehavior/ControladorPJ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Agente;
+using Portador.Implementaciones;
+using UnityEngine;
+
+namespace Monobehavior
+{
+    public class ControladorPJ : MonoBehaviour
+    {
+        private PortadorJugable agente;
+        private void Start()
+        {
+            
+        }
+    }
+}
